15be2cc5216eacdf82c7cb1a51b411a0b1214fcf
Jesús V.G <Jesus777.jsgl@gmail.com>
2022-03-12 16:38:18 -0600

    clases de tailwind ordenados automaticamente

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,12 +39,12 @@
 </head>
 
 <body class="font-sans antialiased">
-	<div class="min-h-screen bg-gray-100 flex flex-col">
+	<div class="flex min-h-screen flex-col bg-gray-100">
 		@include('layouts.navigation')
 
 		<!-- Page Heading -->
 		<header class="bg-gray-800 shadow">
-			<div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+			<div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
 				{{ $header }}
 			</div>
 		</header>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,34 +1,34 @@
-<footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
+<footer class="rounded-lg bg-white p-4 shadow dark:bg-gray-800 md:px-6 md:py-8">
 	<div class="sm:flex sm:items-center sm:justify-between">
 		<a
 			href="https://flowbite.com"
 			target="_blank"
-			class="flex items-center mb-4 sm:mb-0"
+			class="mb-4 flex items-center sm:mb-0"
 		>
-			<img
+			{{-- <img
 				src="/docs/images/logo.svg"
 				class="mr-4 h-8"
 				alt="Flowbite Logo"
-			/>
-			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+			/> --}}
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
 		</a>
-		<ul class="flex flex-wrap items-center mb-6 sm:mb-0">
+		<ul class="mb-6 flex flex-wrap items-center sm:mb-0">
 			<li>
 				<a
 					href="#"
-					class="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
+					class="mr-4 text-sm text-gray-500 hover:underline dark:text-gray-400 md:mr-6"
 				>About</a>
 			</li>
 			<li>
 				<a
 					href="#"
-					class="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
+					class="mr-4 text-sm text-gray-500 hover:underline dark:text-gray-400 md:mr-6"
 				>Privacy Policy</a>
 			</li>
 			<li>
 				<a
 					href="#"
-					class="mr-4 text-sm text-gray-500 hover:underline md:mr-6 dark:text-gray-400"
+					class="mr-4 text-sm text-gray-500 hover:underline dark:text-gray-400 md:mr-6"
 				>Licensing</a>
 			</li>
 			<li>
@@ -39,8 +39,8 @@
 			</li>
 		</ul>
 	</div>
-	<hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-	<span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a
+	<hr class="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
+	<span class="block text-sm text-gray-500 dark:text-gray-400 sm:text-center">© 2022 <a
 			href="https://flowbite.com"
 			target="_blank"
 			class="hover:underline"
